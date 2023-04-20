7860ec7d7bdca9abf8d708e171ec79c52dc68f61
Sam Warren <sw.samwarren@gmail.com>
2023-04-20 22:17:10 +0100

    update

--- modules.py
+++ modules.py
@@ -1,3 +1,12 @@
+# HAndy list of modules at
+# https://docs.python.org/3/py-modindex.html
+# pip package manager should be within 3
+# cmd prompt pip --verions == version 20 something
+# python_docx installed just to check
+
 import tools
+import docx
 
 print(tools.roll_dice(6))
+# docx. to call a docx fnct/var
+# could also pip uninstall pythongfdjkfajbkfa
