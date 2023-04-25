f40e7198afb82f127bcf72b15fc4f943bfd53ca7
Sam Warren <sw.samwarren@gmail.com>
2023-04-25 22:53:55 +0100

    fix

--- flaskwebapp/main.py
+++ flaskwebapp/main.py
@@ -1,10 +1,12 @@
+from noodles.templates import create_app
 # installed flask, flask login, flask sql alchemy using terminal
-from noodles import create_app
 # the init file within website>templates makes the websites folder
 # a python package
 # runs everything within init
 
-app = create_app()
+# note - import error prior to adding .templates  suffix
+
+app = create_app(__name__) # added __name__ to get around error
 
 if __name__ == '__main__':
     # only if we run this file, not import,
--- flaskwebapp/noodles/templates/__init__.py
+++ flaskwebapp/noodles/templates/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-def create_app():
+def create_app(__name__):
     app = Flask(__name__) # initialising flask
     app.config['SECRET_KEY'] = 'randomstringgoeshereshouldneverbeshared' # encrypts session data
     
