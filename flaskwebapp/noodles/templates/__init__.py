229062a838a969168e19ce23a38eaffb0b98ce9f
Sam Warren <sw.samwarren@gmail.com>
2023-04-28 22:55:24 +0100

    add home.html, use template, solve jinja exception

--- flaskwebapp/noodles/templates/__init__.py
+++ flaskwebapp/noodles/templates/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 
 def create_app(__name__):
-    app = Flask(__name__) # initialising flask
+    app = Flask(__name__, template_folder='noodles/templates') # initialising flask
+    # had to define template_folder as was getting jinja2.exceptions.TemplateNotFound exception
     app.config['SECRET_KEY'] = 'randomstringgoeshereshouldneverbeshared' # encrypts session data
     # after setting up the views bit
     from .views import views
--- flaskwebapp/noodles/templates/views.py
+++ flaskwebapp/noodles/templates/views.py
@@ -1,7 +1,7 @@
-from flask import Blueprint # module - bunch of urls inside it
+from flask import Blueprint, render_template # module - bunch of urls inside it
 
 views = Blueprint('views', __name__)
 
 @views.route('/') #define decorator
 def home(): #define function
-    return "<h1>Test</h1>"
+    return render_template("home.html")
