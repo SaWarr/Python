5098497b52aa31a43853bd7b7df652c01e147d24
Sam Warren <sw.samwarren@gmail.com>
2023-05-01 22:03:17 +0100

    update login/signup

--- flaskwebapp/noodles/templates/auth.py
+++ flaskwebapp/noodles/templates/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template 
+from flask import Blueprint, render_template, request, flash 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login') #define decorator
+@auth.route('/login', methods=["GET", "POST"]) #define decorator
 def login(): #define function, often same designation as route
     return render_template("login.html")
 
@@ -10,6 +10,24 @@ def login(): #define function, often same designation as route
 def logout(): 
     return "<p>Logout<p>"
 
-@auth.route('/sign-up') 
+@auth.route('/sign-up', methods=["GET", "POST"]) 
 def signup(): 
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(email) < 4:
+            flash("Your email appears to be invalid.", category="error")
+        elif len(firstName) <= 2:
+            flash("Your first name looks incorrect.", category="error")
+        elif password1 != password2:
+            flash("Your passwords do not seem to match.", category="error")
+        elif len(password1) < 6:
+            flash("Your password needs to be longer.", category="error")
+        else:
+            flash("Account created", category="success")
+            # add user to db
+
     return render_template("sign_up.html")
