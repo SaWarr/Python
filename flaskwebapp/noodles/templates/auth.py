8afd9d81dbb2b34e83f81bc6030d37752d376400
Sam Warren <sw.samwarren@gmail.com>
2023-06-06 22:47:57 +0100

    update - create account

--- flaskwebapp/noodles/templates/auth.py
+++ flaskwebapp/noodles/templates/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash 
+from flask import Blueprint, render_template, request, flash, redirect, url_for  
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -14,20 +17,27 @@ def logout():
 def signup(): 
     if request.method == "POST":
         email = request.form.get("email")
-        firstName = request.form.get("firstName")
+        first_name = request.form.get("first_name")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
         if len(email) < 4:
             flash("Your email appears to be invalid.", category="error")
-        elif len(firstName) <= 2:
+        elif len(first_name) <= 2:
             flash("Your first name looks incorrect.", category="error")
         elif password1 != password2:
             flash("Your passwords do not seem to match.", category="error")
         elif len(password1) < 6:
             flash("Your password needs to be longer.", category="error")
         else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256') )
+            # sha256 just a hashing algorithm
+            db.session.add(new_user)
+            db.session.commit()
+
             flash("Account created", category="success")
             # add user to db
+            return redirect(url_for('views.home'))
+
 
     return render_template("sign_up.html")
